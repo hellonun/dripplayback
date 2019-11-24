b8cda40bf7f70743f2699060c79cb5b136f1f221
hellonun <43077909+hellonun@users.noreply.github.com>
2019-11-24 14:57:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let w;
+let h;
+let capture;
+let img;
+
+function preload() {
+  capture = createVideo('driplong.mp4');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  w = windowWidth;
+  h = windowHeight;
+  // background(255);
+  capture.size(w, h);
+  capture.volume(0.5);
+  capture.play();
+  capture.hide();
+  pixelDensity(1);
+
+}
+
+function draw() {
+  capture.speed(map(capture.time(), 0, capture.duration()/3, 0.6,0.7));
+  image(capture, 0, 0);
+
+}
